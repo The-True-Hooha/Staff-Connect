190ce648940f86448fc44cfac68c73173b41be5b
David Ogar <owogogahhero@outlook.com>
2022-02-05 16:24:52 +0100

    created differnt packages for the project

---
+++ src/main/java/com/TheTrueHooha/StaffConnectApplication.java
@@ -0,0 +1,13 @@
+package com.TheTrueHooha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TheTrueHooha/StaffConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TheTrueHooha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
