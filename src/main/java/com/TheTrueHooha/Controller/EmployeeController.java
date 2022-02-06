879cdd0a27021e0fb2b32cf22fff15a17db2b9e0
David Ogar <owogogahhero@outlook.com>
2022-02-06 16:21:42 +0100

    created a post method that handles new employee data

--- src/main/java/com/TheTrueHooha/Controller/EmployeeController.java
+++ src/main/java/com/TheTrueHooha/Controller/EmployeeController.java
@@ -1,8 +1,28 @@
 package com.TheTrueHooha.Controller;
+//the controller responds created the api endpoint for the http status codes
 
+import com.TheTrueHooha.Model.Employee;
+import com.TheTrueHooha.Service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
 
+    private EmployeeService employeeService;
+
+    //a constructor for the employee service controller
+    public EmployeeController (EmployeeService employeeService) {
+        super();
+        this.employeeService = employeeService;
+    }
+
+    //creates employee rest api
+    @PostMapping //handles post http request
+    public ResponseEntity<Employee> saveEmployee( @RequestBody Employee employee) {
+        return new ResponseEntity<Employee>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+    }
 }
